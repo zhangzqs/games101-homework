ee99b35ee613567d74ac2280d44ba32daeabbe54
zzq <i@zhangzqs.cn>
2025-08-09 14:57:32 +0800

    作业一完成

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -11,49 +11,114 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
-        -eye_pos[2], 0, 0, 0, 1;
+    translate << 1, 0, 0, -eye_pos[0],
+        0, 1, 0, -eye_pos[1],
+        0, 0, 1, -eye_pos[2],
+        0, 0, 0, 1;
 
     view = translate * view;
 
     return view;
 }
 
+Eigen::Matrix4f get_rotation_matrix(Eigen::Vector3f axis, float angle)
+{
+    // 归一化轴向量
+    axis.normalize();
+    // 计算旋转角度的弧度值
+    float rad = angle * MY_PI / 180.0f;
+
+    // n的叉乘矩阵
+    Eigen::Matrix3f n(3, 3);
+    n << 0, -axis.z(), axis.y(),
+        axis.z(), 0, -axis.x(),
+        -axis.y(), axis.x(), 0;
+    // n的外积矩阵
+    Eigen::Matrix3f n_outer = axis * axis.transpose();
+
+    // 计算三部分
+    Eigen::Matrix3f component1, component2, component3;
+    component1 = Eigen::Matrix3f::Identity() * cos(rad);
+    component2 = n * sin(rad);
+    component3 = n_outer * (1 - cos(rad));
+
+    // 组合成旋转矩阵
+    Eigen::Matrix3f rotation_matrix = component1 + component2 + component3;
+
+    // 将3x3旋转矩阵转换为4x4矩阵
+    Eigen::Matrix4f rotation = Eigen::Matrix4f::Identity();
+    rotation.block<3, 3>(0, 0) = rotation_matrix;
+
+    return rotation;
+}
+
 Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
-    // Create the model matrix for rotating the triangle around the Z axis.
-    // Then return it.
-
+    // 实现三维空间中绕z轴旋转的变换矩阵
+    Eigen::Matrix4f rotate;
+    float rad = rotation_angle * MY_PI / 180.0f;
+    rotate << cos(rad), -sin(rad), 0, 0,
+        sin(rad), cos(rad), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+    model = rotate * model;
     return model;
 }
 
+// 参考：
+// https://github.com/DrFlower/GAMES_101_202_Homework/blob/main/Homework_101/Assignment1/Code/main.cpp
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
-    // Students will implement this function
+    assert(zNear > 0 && zFar > 0); // 这里的zNear和zFar是距离，是正数
 
-    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+    // 这里转化成坐标
+    zNear = -zNear;
+    zFar = -zFar;
 
-    // TODO: Implement this function
-    // Create the projection matrix for the given parameters.
-    // Then return it.
+    float t = tan(eye_fov / 2 * MY_PI / 180.0f) * abs(zNear);
+    float r = t * aspect_ratio;
+    float b = -t;
+    float l = -r;
 
+    // 正交投影先归一化到一个[-1,1]^3的立方体
+    Eigen::Matrix4f M_ortho_scale;
+    M_ortho_scale << 2 / (r - l), 0, 0, 0,
+        0, 2 / (t - b), 0, 0,
+        0, 0, 2 / (zFar - zNear), 0,
+        0, 0, 0, 1;
+
+    // 再平移到原点
+    Eigen::Matrix4f M_ortho_translate;
+    M_ortho_translate << 1, 0, 0, -(r + l) / 2,
+        0, 1, 0, -(t + b) / 2,
+        0, 0, 1, -(zNear + zFar) / 2,
+        0, 0, 0, 1;
+
+    // 透视转正交投影矩阵
+    Eigen::Matrix4f M_persp2ortho;
+    M_persp2ortho << zNear, 0, 0, 0,
+        0, zNear, 0, 0,
+        0, 0, zNear + zFar, -zNear * zFar,
+        0, 0, 1, 0;
+    Eigen::Matrix4f projection = M_ortho_scale * M_ortho_translate * M_persp2ortho;
     return projection;
 }
 
-int main(int argc, const char** argv)
+int main(int argc, const char **argv)
 {
     float angle = 0;
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
+    if (argc >= 3)
+    {
         command_line = true;
         angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
+        if (argc == 4)
+        {
             filename = std::string(argv[3]);
         }
     }
@@ -61,6 +126,9 @@ int main(int argc, const char** argv)
     rst::rasterizer r(700, 700);
 
     Eigen::Vector3f eye_pos = {0, 0, 5};
+
+    // 旋转轴向量
+    Eigen::Vector3f rotate_axis = {1, 1, 0};
 
     std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
 
@@ -72,7 +140,8 @@ int main(int argc, const char** argv)
     int key = 0;
     int frame_count = 0;
 
-    if (command_line) {
+    if (command_line)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
@@ -88,10 +157,12 @@ int main(int argc, const char** argv)
         return 0;
     }
 
-    while (key != 27) {
+    while (key != 27)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        // r.set_model(get_model_matrix(angle)); // 围绕z轴旋转的变换矩阵
+        r.set_model(get_rotation_matrix(rotate_axis, angle)); // 绕任意轴旋转的变换矩阵
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
@@ -104,10 +175,14 @@ int main(int argc, const char** argv)
 
         std::cout << "frame count: " << frame_count++ << '\n';
 
-        if (key == 'a') {
+        if (key == 'a')
+        {
+            std::cout << "angle: " << angle << '\n';
             angle += 10;
         }
-        else if (key == 'd') {
+        else if (key == 'd')
+        {
+            std::cout << "angle: " << angle << '\n';
             angle -= 10;
         }
     }
